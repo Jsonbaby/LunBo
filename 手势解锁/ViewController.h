d0c3ecacd80a17d393c3910e404525d2ceb5738b
张璠 <ttxs_code@163.com>
2016-10-31 15:45:01 +0800

    The lock of app

---
+++ 手势解锁/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  手势解锁
+//
+//  Created by 张璠 on 16/10/31.
+//  Copyright © 2016年 张璠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 手势解锁/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  手势解锁
+//
+//  Created by 张璠 on 16/10/31.
+//  Copyright © 2016年 张璠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
